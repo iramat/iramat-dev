8055e661c8127db6f836f7b244adacbfc97b3859
HUET Thomas <TH282424@extra.cea.fr>
2025-04-18 11:02:26 +0200

    varia

--- dbs/chips/code/reference_data.py
+++ dbs/chips/code/reference_data.py
@@ -2,13 +2,27 @@
 
 # %%
 
-import requests
+import requests 
+import tempfile
+import importlib.util
+import os
 
 url = 'https://raw.githubusercontent.com/zoometh/iramat-test-functions/main/chips.py'
 response = requests.get(url)
 
-with open('bdd.py', 'w', encoding='utf-8') as f:
-    f.write(response.text)
+# with open('bdd.py', 'w', encoding='utf-8') as f:
+#     f.write(response.text)
+# Save to a temporary file
+with tempfile.NamedTemporaryFile(suffix=".py", delete=False) as tmp_file:
+    tmp_file.write(response.content)
+    tmp_file_path = tmp_file.name
+
+# Import the module
+module_name = "bdd"
+spec = importlib.util.spec_from_file_location(module_name, tmp_file_path)
+module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(module)
+
 
 # %%
 
