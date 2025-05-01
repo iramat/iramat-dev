eb07558291b65b1f29008a5d85d427d0b87351e5
HUET Thomas <TH282424@extra.cea.fr>
2025-05-01 11:35:00 +0200

    varia

--- dbs/chips/code/run.py
+++ dbs/chips/code/run.py
@@ -7,24 +7,4 @@ db_upsert(data_entry= root_path + "dbs/chips/data/import_tableEchantillons_test.
 
 
 # %%
-# TODO: install conda and then datatable
-from bdd import db_connect, db_query
 
-root_path = "C:/Users/TH282424/Rprojects/iramat-test/"
-out_file = root_path + "dbs/chips/data/reference_data/machines.tsv"
-engine = db_connect(root_path + "credentials/pg_dev_credentials.json")
-conn = engine.raw_connection()
-cur = conn.cursor
-
-table="machines"
-excl_field="id_dispositif"
-
-df=db_query(query = f"SELECT * FROM {table};", engine=engine)
-df = df.drop(excl_field, axis=1)
-df.to_csv(out_file, sep="\t") 
-
-# df.head()
-sub = dt.Frame(df)
-
-
-# %%
