24f6467d965d94ac26aed6fd7c9c8fad20f79e9c
DISSER Alexandre <adisser@extra.cea.fr>
2025-05-21 15:09:27 +0200

    add_SQL_query

--- dbs/chips/code/run.py
+++ dbs/chips/code/run.py
@@ -1,6 +1,6 @@
 from bdd import db_connect, db_upsert, db_tabi
 
-root_path = "C:/Users/TH282424/Rprojects/iramat-test/"
+root_path = "C:/Users/adisser/Git_repo/iramat-test/"
 engine = db_connect(root_path + "credentials/pg_dev_credentials.json")
-db_upsert(data_entry= root_path + "dbs/chips/data/import_tableEchantillons_test.csv",
-                table="echantillons", separator = ',', engine=engine, verbose = True)
+db_upsert(data_entry= root_path + "dbs/chips/data/import_tableChips_test1.csv",
+                table="chips", separator = ',', engine=engine, verbose = True)
