6f3bb3a6e957b86b6046191674c6f93daf8bdae1
HUET Thomas <TH282424@extra.cea.fr>
2025-06-03 15:34:01 +0200

    varia

--- dbs/chips/code/run.py
+++ dbs/chips/code/run.py
@@ -5,23 +5,24 @@ from bdd import db_connect, db_upsert, db_tabi
 # root_path = "C:/Users/adisser/Git_repo/iramat-test/"
 root_path = "C:/Users/TH282424/Rprojects/iramat-dev/"
 
-#%% Upsert table 'chips' (chips_d DB, development database)
+#%% UPSERT table 'chips' (chips_d DB, development database)
 
 table = 'chips'
 engine = db_connect(root_path + "credentials/pg_dev_credentials.json")
 db_upsert(data_entry= root_path + "dbs/chips/data/_import_tableChips_test.csv",
                 table=table, separator = ',', engine=engine, verbose = True)
 
-#%% Upsert table 'echantillons' (chips_d DB, development database)
+#%% UPSERT table 'echantillons' (chips_d DB, development database)
 
 table = 'echantillons'
 engine = db_connect(root_path + "credentials/pg_dev_credentials.json")
 db_upsert(data_entry= root_path + "dbs/chips/data/_import_tableEchantillons_test.csv",
                 table=table, separator = ',', engine=engine, verbose = True)
 
-# %%
+# %% UPSERT table 'sites' (chips_d DB, development database)
 
 table = 'sites'
 engine = db_connect(root_path + "credentials/pg_dev_credentials.json")
 db_upsert(data_entry= root_path + "dbs/chips/data/_import_tableSites_test.csv",
                 table=table, separator = ',', engine=engine, verbose = True)
+# %%
