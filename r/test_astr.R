c534874e429615437a13ef4b973db52d83ab7134
HUET Thomas <TH282424@extra.cea.fr>
2026-06-15 11:44:12 +0200

    added margins to IRAMAT logo

--- r/test_astr.R
+++ r/test_astr.R
@@ -4,4 +4,5 @@ library(ASTR)
 # reading an ASTR table directly from a file
 test_file <- system.file("extdata", "test_data_input_good.csv", package = "ASTR")
 arch <- read_ASTR(test_file, id_column = "Sample", context = 1:7)
+cat(colnames(arch), sep = "\n")
 knitr::kable(head(arch, 4), format = "pipe")
